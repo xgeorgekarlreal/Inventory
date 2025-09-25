79f95bfee0e34e0afd152a20ff5f567a54067dcc
xgeorgekarlreal <xgeorgekarlreal@gmail.com>
2025-09-25 21:18:00 +0800

    Updated CategoriesPage.tsx

--- src/pages/management/CategoriesPage.tsx
+++ src/pages/management/CategoriesPage.tsx
@@ -1,4 +1,4 @@
-```typescript
+typescript
 import React, { useState, useEffect } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../../contexts/AuthContext'
@@ -305,4 +305,3 @@ const CategoriesPage: React.FC = () => {
 }
 
 export default CategoriesPage
-```
